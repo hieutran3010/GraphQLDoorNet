c956d64ca1aa7a1c6b3aa8b9bb5bad0248be46fd
Hieu Tran <trantrunghieu3010@gmail.com>
2020-07-10 18:40:48 +0700

    Protected for UnitOfWork of EntityQueryBase and Bump to version 1.1.2

--- EntityQueryBase.cs
+++ EntityQueryBase.cs
@@ -11,28 +11,28 @@ namespace GraphQLDoorNet
 
     public class EntityQueryBase<T> where T : class, IEntityBase, new()
     {
-        private readonly IUnitOfWork unitOfWork;
+        protected readonly IUnitOfWork UnitOfWork;
 
         public EntityQueryBase(IUnitOfWork unitOfWork)
         {
-            this.unitOfWork = unitOfWork;
+            this.UnitOfWork = unitOfWork;
         }
 
         public virtual Task<T> GetById(Guid id)
         {
-            var repository = this.unitOfWork.GetRepository<T>();
+            var repository = this.UnitOfWork.GetRepository<T>();
             return  repository.GetQueryable().FirstOrDefaultAsync(entity => entity.Id == id);
         }
 
         public virtual Task<T> QueryOne(QueryParams queryParams)
         {
-            var repository = this.unitOfWork.GetRepository<T>();
+            var repository = this.UnitOfWork.GetRepository<T>();
             return repository.GetQueryable().ApplyQueryOneParams(queryParams);
         }
 
         public virtual Task<List<T>> QueryMany(QueryParams queryParams)
         {
-            var repository = this.unitOfWork.GetRepository<T>();
+            var repository = this.UnitOfWork.GetRepository<T>();
             return repository.GetQueryable().ApplyQueryParams(queryParams).ToListAsync();
         }
 
@@ -41,8 +41,8 @@ namespace GraphQLDoorNet
             var result = new CountResult
             {
                 NumberOfItem = string.IsNullOrWhiteSpace(query)
-                    ? await this.unitOfWork.GetRepository<T>().GetQueryable().CountAsync()
-                    : await Task.Run(() => this.unitOfWork.GetRepository<T>().GetQueryable().Count(query))
+                    ? await this.UnitOfWork.GetRepository<T>().GetQueryable().CountAsync()
+                    : await Task.Run(() => this.UnitOfWork.GetRepository<T>().GetQueryable().Count(query))
             };
             return result;
         }
